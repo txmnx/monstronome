f4bdeb8b077c72d076fc098dd19539838d58895d
terrypg34 <56301490+terrypg34@users.noreply.github.com>
2020-04-06 14:07:19 +0200

    [ADD] Wwise integration
    ADD wwise 2019.2.0 SDK integration

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,137 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_MUSIC = 2932040671U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace GAME_STATE
+        {
+            static const AkUniqueID GROUP = 766723505U;
+
+            namespace STATE
+            {
+                static const AkUniqueID END = 529726532U;
+                static const AkUniqueID MIDDLE = 1026627430U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID START = 1281810935U;
+                static const AkUniqueID TENSE = 391998042U;
+            } // namespace STATE
+        } // namespace GAME_STATE
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace SW_INTENTION_BRASS
+        {
+            static const AkUniqueID GROUP = 1090601500U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID PIZZICATO = 2077337834U;
+                static const AkUniqueID STACCATO = 56434811U;
+                static const AkUniqueID SUSTAIN = 4208866100U;
+            } // namespace SWITCH
+        } // namespace SW_INTENTION_BRASS
+
+        namespace SW_INTENTION_PERCUSSIONS
+        {
+            static const AkUniqueID GROUP = 4144689221U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID PIZZICATO = 2077337834U;
+                static const AkUniqueID STACCATO = 56434811U;
+                static const AkUniqueID SUSTAIN = 4208866100U;
+            } // namespace SWITCH
+        } // namespace SW_INTENTION_PERCUSSIONS
+
+        namespace SW_INTENTION_STRINGS
+        {
+            static const AkUniqueID GROUP = 4251027333U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID PIZZICATO = 2077337834U;
+                static const AkUniqueID STACCATO = 56434811U;
+                static const AkUniqueID SUSTAIN = 4208866100U;
+            } // namespace SWITCH
+        } // namespace SW_INTENTION_STRINGS
+
+        namespace SW_INTENTION_WOODS
+        {
+            static const AkUniqueID GROUP = 4012344001U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID PIZZICATO = 2077337834U;
+                static const AkUniqueID STACCATO = 56434811U;
+                static const AkUniqueID SUSTAIN = 4208866100U;
+            } // namespace SWITCH
+        } // namespace SW_INTENTION_WOODS
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID RTPC_ARTICULATION_ALL = 2786603936U;
+        static const AkUniqueID RTPC_ARTICULATION_BRASS = 2436025794U;
+        static const AkUniqueID RTPC_ARTICULATION_PERCUSSIONS = 2880407975U;
+        static const AkUniqueID RTPC_ARTICULATION_STRINGS = 2116129127U;
+        static const AkUniqueID RTPC_ARTICULATION_WOODS = 1437958447U;
+        static const AkUniqueID RTPC_INTENSITY_ALL = 2493655934U;
+        static const AkUniqueID RTPC_INTENSITY_BRASS = 3154931128U;
+        static const AkUniqueID RTPC_INTENSITY_PERCUSSIONS = 2676178657U;
+        static const AkUniqueID RTPC_INTENSITY_STRINGS = 234013449U;
+        static const AkUniqueID RTPC_INTENSITY_WOODS = 1164038749U;
+        static const AkUniqueID RTPC_TEMPO = 3945680200U;
+        static const AkUniqueID RTPC_TIME_DELAY_BRASS = 666250906U;
+        static const AkUniqueID RTPC_TIME_DELAY_PERCUSSIONS = 4041181711U;
+        static const AkUniqueID RTPC_TIME_DELAY_STRINGS = 3714523071U;
+        static const AkUniqueID RTPC_TIME_DELAY_WOODS = 3650061639U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MONSTRONOME_SNB = 3550178992U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID AMBISONICS_BUS = 3539010900U;
+        static const AkUniqueID BRASS = 505915784U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID PERCUSSIONS = 3762497105U;
+        static const AkUniqueID STRINGS = 3363523641U;
+        static const AkUniqueID WOOD = 2058049674U;
+    } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID REVERB_CONCERTHALL = 44205819U;
+    } // namespace AUX_BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
