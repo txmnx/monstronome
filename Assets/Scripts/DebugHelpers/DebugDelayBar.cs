ae7fa916e16e00289968d5ffe59e98d8f87b03e1
ahprog <herrero.antonin@gmail.com>
2020-04-13 00:26:36 +0200

    (debug) Ajout de prefabs Delay Bar

---
+++ Assets/Scripts/DebugHelpers/DebugDelayBar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DebugDelayBar : MonoBehaviour
+{
+    public TextMeshPro delayTextMesh;
+    public Transform bar;
+
+    public void SetValue(float value)
+    {
+        delayTextMesh.text = value.ToString();
+        bar.localScale = new Vector3(
+            value / SoundEngineTuner.MAX_DELAY,
+            bar.localScale.y,
+            bar.localScale.z
+        );
+    }
+}
