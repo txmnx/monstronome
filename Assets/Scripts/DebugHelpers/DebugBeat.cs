876451cb8336ac87088162effc601711ec3cdd01
ahprog <herrero.antonin@gmail.com>
2020-05-12 18:53:19 +0200

    (feat) Ajout d'un sprite de cercle à la place du beat plane pour le debug

--- Assets/Scripts/DebugHelpers/DebugBeat.cs
+++ Assets/Scripts/DebugHelpers/DebugBeat.cs
@@ -11,20 +11,25 @@ public class DebugBeat : MonoBehaviour
     public BeatManager beatManager;
     public MeshRenderer panelRenderer;
     public Material onBeatMaterial;
+    public Color onBeatCircleColor;
     public TextMeshPro amplitudeText;
+    public SpriteRenderer circleBeat;
 
     private Material m_DefaultMaterial;
+    private Color m_DefaultCircleColor;
 
     private void Start()
     {
         beatManager.OnBeatMajorHand += OnBeatMajorHand;
         m_DefaultMaterial = panelRenderer.material;
+        m_DefaultCircleColor = circleBeat.color;
     }
 
     public void OnBeatMajorHand(float amplitude)
     {
         amplitudeText.text = amplitude.ToString();
         panelRenderer.material = onBeatMaterial;
+        circleBeat.color = onBeatCircleColor;
         StartCoroutine(OnBeatEnd());
     }
 
@@ -32,5 +37,6 @@ public class DebugBeat : MonoBehaviour
     {
         yield return new WaitForSeconds(0.1f);
         panelRenderer.material = m_DefaultMaterial;
+        circleBeat.color = m_DefaultCircleColor;
     }
 }
