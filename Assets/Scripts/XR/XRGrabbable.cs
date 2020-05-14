dcb546067203884d973bd7e1d13b9066d6d711e4
ahprog <herrero.antonin@gmail.com>
2020-05-14 18:46:45 +0200

    (feat) XRGrabbable & XRGrabber

---
+++ Assets/Scripts/XR/XRGrabbable.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Represents a game object that can be grabbed
+ */
+[RequireComponent(typeof(Rigidbody))]
+public class XRGrabbable : MonoBehaviour
+{
+    private Rigidbody rb;
+    private bool m_UseGravity;
+    private bool m_IsKinematic;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    
+    public void OnEnterGrab()
+    {
+        m_UseGravity = rb.useGravity;
+        m_IsKinematic = rb.isKinematic;
+    }
+
+    public void OnExitGrab()
+    {
+        rb.useGravity = m_UseGravity;
+        rb.isKinematic = m_IsKinematic;
+    }
+
+    public void Highlight()
+    {
+        
+    }
+    
+    public void RemoveHighlight()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/XR/XRGrabber.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+/**
+ * Component to grab XRGrabbable objects
+ */
+[RequireComponent(typeof(XRCustomController))]
+[RequireComponent(typeof(Collider))]
+public class XRGrabber : MonoBehaviour
+{
+    private XRCustomController m_Controller;
+
+    private XRGrabbable m_HoveredObject;
+    private XRGrabbable m_GrabbedObject;
+
+    private bool m_IsHovering = false;
+    private bool m_IsGrabbing = false;
+
+    private void Start()
+    {
+        m_Controller = GetComponent<XRCustomController>();
+    }
+
+    private void Update()
+    {
+        bool triggerPressed;
+        if (m_Controller.inputDevice.TryGetFeatureValue(CommonUsages.triggerButton, out triggerPressed)) {
+            if (triggerPressed) {
+            }
+            else {
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!m_IsGrabbing) {
+            m_HoveredObject = other.GetComponent<XRGrabbable>();
+            if (m_HoveredObject) {
+                m_HoveredObject.Highlight();
+                m_IsHovering = true;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!m_IsGrabbing) {
+            m_HoveredObject = other.GetComponent<XRGrabbable>();
+            if (m_HoveredObject) {
+                m_HoveredObject.RemoveHighlight();
+                m_IsHovering = false;
+            }
+        }
+    }
+}
