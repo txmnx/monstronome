19f3a1863c9c9fab872d76b6a4437d6e10d256d6
ahprog <herrero.antonin@gmail.com>
2020-05-16 07:06:01 +0200

    (feat) Explosion quand la potion se brise

--- Assets/Scripts/Gameplay/Potion.cs
+++ Assets/Scripts/Gameplay/Potion.cs
@@ -8,32 +8,44 @@ public class Potion : MonoBehaviour
 {
     public Transform defaultBottle;
     public Transform breakedBottle;
+
+    [Header("VFX")]
+    public Transform particlesAnimation;
     
     [Header("Sound")]
     public AK.Wwise.Event SFXOnPotionBreak;
     public AK.Wwise.Event SFXOnPotionCollision;
 
     private Rigidbody m_Rigidbody;
+    private Rigidbody[] m_RigidbodyPieces;
+    private ParticleSystem[] m_ParticleSystems;
 
     private void Awake()
     {
         m_Rigidbody = GetComponent<Rigidbody>();
+        m_RigidbodyPieces = breakedBottle.GetComponentsInChildren<Rigidbody>();
+        m_ParticleSystems = GetComponentsInChildren<ParticleSystem>();
     }
 
     private void OnCollisionEnter(Collision other)
     {
         float speed = m_Rigidbody.velocity.magnitude + (m_Rigidbody.angularVelocity.magnitude / 10);
         
-        if (speed > 2.5f) {
+        if (speed > 1.75f) {
             breakedBottle.gameObject.SetActive(true);
             
-            Rigidbody[] rbsPieces = breakedBottle.GetComponentsInChildren<Rigidbody>();
             float explosionForce = speed * speed;
-            foreach (Rigidbody rb in rbsPieces) {
+            foreach (Rigidbody rb in m_RigidbodyPieces) {
                 rb.AddExplosionForce(explosionForce, breakedBottle.position, 2.0f, 15.0f);
             }
-            
             m_Rigidbody.isKinematic = true;
+            
+            
+            foreach (ParticleSystem ps in m_ParticleSystems) {
+                ps.gameObject.SetActive(true);
+                ps.Play();
+            }
+            
             SFXOnPotionBreak?.Post(gameObject);
             
             Destroy(defaultBottle.gameObject);
