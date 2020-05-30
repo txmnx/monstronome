1c038f7a1897bafc06f9f22d734781281b25af28
ahprog <herrero.antonin@gmail.com>
2020-05-31 00:24:45 +0200

    (feat) Timeline avec interpolation du timer

--- Assets/Scripts/Gameplay/Timeline.cs
+++ Assets/Scripts/Gameplay/Timeline.cs
@@ -11,16 +11,32 @@ public class Timeline : MonoBehaviour
 
     private Vector3 m_TimelineDirection;
     private float m_TimelineSize;
+
+    private Vector3 m_AimingPosition;
     
     private void Start()
     {
         m_TimelineDirection = end.localPosition - start.localPosition;
         m_TimelineSize = m_TimelineDirection.magnitude;
         m_TimelineDirection = m_TimelineDirection.normalized;
+
+        m_AimingPosition = end.localPosition;
     }
 
-    public void MoveCursor(float t)
+    private void Update()
     {
-        cursor.localPosition += m_TimelineDirection * m_TimelineSize * t;
+        MoveCursor(0.01f * Time.deltaTime);
+        cursor.localPosition = Vector3.Lerp(cursor.localPosition, m_AimingPosition, 0.5f);
+    }
+
+    public void MoveCursor(float percent)
+    {
+        m_AimingPosition = cursor.localPosition + m_TimelineSize * percent * m_TimelineDirection;
+    }
+
+    public void SetCursor(float percent)
+    {
+        cursor.localPosition = start.localPosition + m_TimelineSize * percent * m_TimelineDirection;
+        m_AimingPosition = cursor.localPosition;
     }
 }
