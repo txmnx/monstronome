933267be32cd558eb1fc1b4af6f33f57e500b6a1
Wissam Azzi <wissam.azzi@outlook.fr>
2020-06-03 17:15:04 +0200

    (feat) Lien Leviers et sliderValue
    - clamp de la valeur mais pas de blocage de la position du levier dans l'espace
    - valeur utilisable avec le script de Terry

---
+++ Assets/Scripts/Gameplay/Soundboard/SoundboardSlideControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SoundboardSlideControl : MonoBehaviour
+{
+
+    public GameObject positionAt0;
+
+    public GameObject positionAt1;
+
+    public float sliderValue;
+
+    private Vector3 pos;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        sliderValue = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pos = transform.position;
+        sliderValue = Mathf.InverseLerp(positionAt0.transform.position.x, positionAt1.transform.position.x, pos.x);
+        Mathf.Clamp(sliderValue, 0, 1);
+    }
+}
