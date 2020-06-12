3e04d94ce8c44b904a6da1dbb7b6263ab8800b91
ahprog <herrero.antonin@gmail.com>
2020-06-12 05:45:14 +0200

    (feat) Feedback haptique et SFX sur le toaster

--- Assets/Scripts/UI/ToastsSlider.cs
+++ Assets/Scripts/UI/ToastsSlider.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,22 +8,57 @@ using UnityEngine;
  */
 public class ToastsSlider : XRSlider
 {
-    [Header("Toasts")] 
+    [Header("Toasts")]
     public Animator[] animators = new Animator[3];
+    private int m_ShowPropertyID;
+        
+    [Header("SFX")]
+    public GameObject toasterSoundReference;
+    public AK.Wwise.Event SFXOnToasterIn;
+    public AK.Wwise.Event SFXOnToasterOut;
+
+    private bool m_IsOnExtremity = false;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        m_ShowPropertyID = Animator.StringToHash("show");
+    }
 
     public override void OnUpdateGrab(XRGrabber xrGrabber)
     {
         base.OnUpdateGrab(xrGrabber);
 
-        if (value > 0.98f) {
-            foreach (Animator animator in animators) {
-                animator.SetBool("show", true);
+        if (value > 0.99f) {
+            if (!m_IsOnExtremity) {
+                if (!animators[0].GetBool(m_ShowPropertyID)) {
+                    SFXOnToasterOut.Post(toasterSoundReference);
+                }
+                xrGrabber.HapticImpulse(0.4f, 0.01f);   
             }
+            
+            foreach (Animator animator in animators) {
+                animator.SetBool(m_ShowPropertyID, true);
+            }
+            
+            m_IsOnExtremity = true;
         }
-        else if (value < 0.02f) {
-            foreach (Animator animator in animators) {
-                animator.SetBool("show", false);
+        else if (value < 0.01f) {
+            if (!m_IsOnExtremity) {
+                if (animators[0].GetBool(m_ShowPropertyID)) {
+                    SFXOnToasterIn.Post(toasterSoundReference);
+                }
+                xrGrabber.HapticImpulse(0.4f, 0.01f);   
             }
+            
+            foreach (Animator animator in animators) {
+                animator.SetBool(m_ShowPropertyID, false);
+            }
+            
+            m_IsOnExtremity = true;
+        }
+        else {
+            m_IsOnExtremity = false;
         }
     }
 }
