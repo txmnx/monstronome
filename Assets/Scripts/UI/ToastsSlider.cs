fc2149c0bff148f8f7465a7d82ff1902b26868e3
ahprog <herrero.antonin@gmail.com>
2020-06-12 05:31:10 +0200

    (feat) Animations toasts avec slider toaster

---
+++ Assets/Scripts/UI/ToastsSlider.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * XRSlider for the toaster
+ */
+public class ToastsSlider : XRSlider
+{
+    [Header("Toasts")] 
+    public Animator[] animators = new Animator[3];
+
+    public override void OnUpdateGrab(XRGrabber xrGrabber)
+    {
+        base.OnUpdateGrab(xrGrabber);
+
+        if (value > 0.98f) {
+            foreach (Animator animator in animators) {
+                animator.SetBool("show", true);
+            }
+        }
+        else if (value < 0.02f) {
+            foreach (Animator animator in animators) {
+                animator.SetBool("show", false);
+            }
+        }
+    }
+}
