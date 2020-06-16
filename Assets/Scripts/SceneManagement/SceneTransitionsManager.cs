4fe0bd17ee86f70453b363d483317ae351179d80
ahprog <herrero.antonin@gmail.com>
2020-06-16 23:10:34 +0200

    (feat) Transition scene basique

---
+++ Assets/Scripts/SceneManagement/SceneTransitionsManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitionsManager : MonoBehaviour
+{
+    [Header("Persistent Items")]
+    public GameObject wwiseBank;
+    
+    
+    private void Awake()
+    {
+        DontDestroyOnLoad(wwiseBank);
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void LoadScene(string scene)
+    {
+        Debug.Log("== LOADING SCENE : " + scene + " ==");
+        StartCoroutine(LoadAsyncScene(scene));
+    }
+
+    private IEnumerator LoadAsyncScene(string scene)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(scene);
+
+        while (!asyncLoad.isDone) {
+            yield return null;
+        }
+    }
+}
