239f045f4b5386cb0374ca68053eb1f1c4a4d73a
ahprog <herrero.antonin@gmail.com>
2020-06-17 05:01:14 +0200

    (feat) Highlight selection TV

--- Assets/Scripts/UI/TV/TVChoice.cs
+++ Assets/Scripts/UI/TV/TVChoice.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
@@ -6,6 +7,18 @@ using UnityEngine.Events;
 public class TVChoice : MonoBehaviour
 {
     public UnityEvent OnSelect;
-    
-    
+    public GameObject defaultChoice;
+    public GameObject highlightChoice;
+
+    public void Highlight(bool highlight)
+    {
+        if (highlight) {
+            defaultChoice.SetActive(false);
+            highlightChoice.SetActive(true);
+        }
+        else {
+            highlightChoice.SetActive(false);
+            defaultChoice.SetActive(true);
+        }
+    }
 }
--- Assets/Scripts/XR/XRTVController.cs
+++ Assets/Scripts/XR/XRTVController.cs
@@ -9,6 +9,10 @@ public class XRTVController : MonoBehaviour
 {
     public Transform controllerTop;
     public LineRenderer rayRenderer;
+
+    private Collider m_CachedCollider;
+    private TVChoice m_PrevChoice;
+    private bool m_HasChoice;
     
     private const int LAYER_MASK_TV = 1 << 9;
     
@@ -17,14 +21,37 @@ public class XRTVController : MonoBehaviour
         if (Physics.Raycast(controllerTop.position, controllerTop.forward, out RaycastHit hit, 10, LAYER_MASK_TV)) {
             rayRenderer.enabled = true;
             rayRenderer.SetPosition(1, rayRenderer.transform.InverseTransformPoint(hit.point));
-
-            if (hit.collider.CompareTag("TVChoice")) {
-                
-            }
             
+            if (hit.collider != m_CachedCollider) {
+                if (hit.collider.CompareTag("TVChoice")) {
+                    TVChoice choice = hit.collider.GetComponent<TVChoice>();
+                    if (m_HasChoice) {
+                        m_PrevChoice.Highlight(false);
+                    }
+                    choice.Highlight(true);
+
+                    m_HasChoice = true;
+                    m_PrevChoice = choice;
+                }
+                else {
+                    if (m_HasChoice) {
+                        m_PrevChoice.Highlight(false);
+                        m_PrevChoice = null;
+                    }
+                    m_HasChoice = false;
+                }
+                
+                m_CachedCollider = hit.collider;
+            }
         }
         else {
             rayRenderer.enabled = false;
+            
+            if (m_HasChoice) {
+                m_PrevChoice.Highlight(false);
+                m_PrevChoice = null;
+            }
+            m_HasChoice = false;
         }
     }
 }
