a365751aae0e7384330d273eae58ace73969e288
ahprog <herrero.antonin@gmail.com>
2020-06-17 03:46:22 +0200

    (feat) Rayon quand on cible la TV

---
+++ Assets/Scripts/XR/XRTVController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Used to control a TV with raycast + trigger
+ */
+public class XRTVController : MonoBehaviour
+{
+    public Transform controllerTop;
+    public LineRenderer rayRenderer;
+    
+    private const int LAYER_MASK_TV = 1 << 9;
+    
+    private void Update()
+    {
+        if (Physics.Raycast(controllerTop.position, controllerTop.forward, out RaycastHit hit, 10, LAYER_MASK_TV)) {
+            rayRenderer.enabled = true;
+            rayRenderer.SetPosition(1, rayRenderer.transform.InverseTransformPoint(hit.point));
+        }
+        else {
+            rayRenderer.enabled = false;
+        }
+    }
+}
