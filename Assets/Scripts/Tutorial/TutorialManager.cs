032f8e07a3b53b926b4382a2d5d495d63035650a
ahprog <herrero.antonin@gmail.com>
2020-07-01 17:45:16 +0200

    (feat) Atlas des TutorialStepsé

--- Assets/Scripts/Tutorial/TutorialManager.cs
+++ Assets/Scripts/Tutorial/TutorialManager.cs
@@ -11,12 +11,20 @@ public class TutorialManager : MonoBehaviour
     public GameObject factory;
     public GameObject potions;
     public GameObject metronomicon;
+
+    private TutorialSequence m_Sequence;
+
+    [Header("Voice instructions")] 
+    [SerializeField]
+    private TutorialStep.Instruction[] m_Instructions;
     
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_Sequence = new TutorialSequence(new TutorialStep[] {
+            //{ new TutorialStep(m_Sequence, ) }
+        });
     }
 
     // Update is called once per frame
--- Assets/Scripts/Tutorial/TutorialStep.cs
+++ Assets/Scripts/Tutorial/TutorialStep.cs
@@ -9,23 +9,29 @@ using UnityEngine;
 public class TutorialStep
 {
     [Serializable]
-    public struct TutorialInstruction
+    public struct Instruction
+    {
+        public TutorialVoice mainInstruction;
+        public TutorialVoice secondInstruction;
+    }
+    
+    [Serializable]
+    public struct TutorialVoice
     {
         public AK.Wwise.Event SFXVoice;
+        [TextArea(1, 5)]
         public string subtitles;
     }
 
     private TutorialSequence m_Sequence;
-    private TutorialInstruction m_MainInstruction;
-    private TutorialInstruction m_SecondInstruction;
+    private Instruction m_Instruction;
     private Action m_SuccessEvent;
     private GameObject[] m_NeededObjects;
 
-    public TutorialStep(TutorialSequence sequence, TutorialInstruction mainInstruction, TutorialInstruction secondInstruction, Action successEvent, GameObject[] neededObjects = null)
+    public TutorialStep(TutorialSequence sequence, Instruction instruction, Action successEvent, GameObject[] neededObjects = null)
     {
         m_Sequence = sequence;
-        m_MainInstruction = mainInstruction;
-        m_SecondInstruction = secondInstruction;
+        m_Instruction = instruction;
         m_SuccessEvent = successEvent;
 
         if (neededObjects != null) {
@@ -46,5 +52,6 @@ public class TutorialStep
     {
         //Here we can "stop" some processes that were only necessary during this tutorial step
         m_Sequence.MoveToNextStep();
+        m_SuccessEvent -= OnSuccess;
     }
 }
