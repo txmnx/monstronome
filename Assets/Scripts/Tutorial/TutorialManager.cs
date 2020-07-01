34ac93248dee392b1b4264a2eb17c05005449324
ahprog <herrero.antonin@gmail.com>
2020-07-01 16:02:35 +0200

    (refactor) Script TutorialManager

---
+++ Assets/Scripts/Tutorial/TutorialManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Describe and check the steps of the tutorial 
+ */
+public class TutorialManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
